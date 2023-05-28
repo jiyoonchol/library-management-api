125fd653bda7298c37574f4c96a84f93086ae923
taro <jiyoon276479@gmail.com>
2023-05-28 17:18:01 +0900

    InMemory User 인증

--- src/main/java/com/Solo/LibraryManagement/security/config/SecurityConfiguration.java
+++ src/main/java/com/Solo/LibraryManagement/security/config/SecurityConfiguration.java
@@ -4,8 +4,11 @@ package com.Solo.LibraryManagement.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -45,6 +48,17 @@ public class SecurityConfiguration {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("taro@gmail.com")
+                        .password("1234")
+                        .roles("USER")
+                        .build()
+                return new InMemoryUserDetailsManager(user);
     }
 
 }
